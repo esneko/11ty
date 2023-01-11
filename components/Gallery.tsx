5eee9abbf7747ace9f90eba064cf799483768400
Andrejs Agejevs <andrejs.agejevs@ideaportriga.com>
2023-01-11 15:39:56 +0200

    JSON assert

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,9 +1,18 @@
+import works from '../_data/works.json' assert { type: "json" }
 import { Cart } from './Cart.js'
 
 const Gallery = () => (
   <>
     <h1>Gallery</h1>
     <Cart total={22} />
+    {works.map((work) => (
+      <div>
+        <h3>{work.title}</h3>
+        {work.pictures.map((picture) => (
+          <img src={picture.src} alt={picture.alt}></img>
+        ))}
+      </div>
+    ))}
   </>
 )
 
