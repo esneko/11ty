1ecf90406780acba7f7fdd7047c2342913db584e
Andrejs Agejevs <andrejs.agejevs@ideaportriga.com>
2023-01-12 12:24:00 +0200

    Linter pass

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,15 +1,21 @@
-import works from '../_data/works.json' assert { type: "json" }
+import works from '../_data/works.json' assert { type: 'json' }
 import { Cart } from './Cart.js'
 
 const Gallery = () => (
   <>
     <h1>Gallery</h1>
     <Cart total={22} />
-    {works.map((work) => (
-      <div>
+    {works.map((work, i) => (
+      <div key={`work_${i}`}>
         <h3>{work.title}</h3>
-        {work.pictures.map((picture) => (
-          <img src={picture.src} alt={picture.alt}></img>
+        {work.pictures.map((picture, i) => (
+          <img
+            key={`picture_${i}`}
+            src={picture.src}
+            alt={picture.alt}
+            height="auto"
+            width="auto"
+          ></img>
         ))}
       </div>
     ))}
